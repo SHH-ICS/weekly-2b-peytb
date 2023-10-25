7e69e90c1af1eea2ff3a05b9c0fa9ba1508fb3ce
peytb <144241899+peytb@users.noreply.github.com>
2023-10-25 08:16:08 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -6,4 +6,4 @@ print("The area of the circle is:", area)
 print("Enter the diameter of your circle")
 d=int(input())
 circumference=3.14*d
-print("The area of the circle is:", area)
+print("The circumference of the circle is:", circumference)
